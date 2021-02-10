134ea6428beb9cada3fea1b906f0965e75a27e60
juring <zlxlwnfld@gmail.com>
2021-02-10 23:55:19 +0900

    프로젝트 재설정

---
+++ Member/Member/src/test/kotlin/com/juri/TypingGameWorld/Member/MemberApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.juri.TypingGameWorld.Member
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MemberApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
