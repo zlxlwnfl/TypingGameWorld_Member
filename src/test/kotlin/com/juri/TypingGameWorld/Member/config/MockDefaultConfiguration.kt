7ba1ccd3d0d1cea4c56b64864c884427f302b2da
juring <zlxlwnfl@naver.com>
2022-01-12 20:43:45 +0900

    #3 테스트 시 내장 mongodb 의존성 exclude

--- src/test/kotlin/com/juri/TypingGameWorld/Member/config/MockDefaultConfiguration.kt
+++ src/test/kotlin/com/juri/TypingGameWorld/Member/config/MockDefaultConfiguration.kt
@@ -4,6 +4,7 @@ import com.juri.TypingGameWorld.Member.global.config.MongoConfig
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveDataAutoConfiguration
 import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.test.mock.mockito.MockBean
 
@@ -11,6 +12,7 @@ import org.springframework.boot.test.mock.mockito.MockBean
 @EnableAutoConfiguration(exclude = [
     MongoReactiveAutoConfiguration::class,
     MongoReactiveDataAutoConfiguration::class,
+    EmbeddedMongoAutoConfiguration::class
 ])
 class MockDefaultConfiguration {
 
